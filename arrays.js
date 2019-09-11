653831f9206363b14b5de2ba04a9e6ae9efd68e2
witty-cache-6360 <silpadamarnie@gmail.com>
2019-09-11 20:17:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,4 +5,4 @@ addElementToBeginningOfArray;
 destructivelyAddElementToBeginningOfArray;
  accessElementInArray;
  destructivelyRemoveElementFromBeginningOfArray;
- 
+ removeElementFromBeginningOfArray;
